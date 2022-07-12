e2f5ce33f72f7c89ad659aaee85569be5be62b25
Sophia Lilienthal <sophialilienthal@icloud.com>
2022-07-11 21:14:22 -0600

    Improved documentation. Added type casting to database.py. Added full db functionality.

--- database.py
+++ database.py
@@ -3,10 +3,10 @@ class Database:
     def __init__(self):
         self._db = {}
 
-    def get_db(self):
+    def get_db(self) -> dict:
         return self._db
 
-    def set_db(self, key, value=None):
+    def set_db(self, key: str, value: dict=None) -> None:
         if value is None:
             self._db[key] = {}
         self._db[key] = value
--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from bs4 import BeautifulSoup
 import requests
 from database import Database
 
+# Create DB object
+# Keys: Product titles Values: Dicts containing spec key/val pairs
+db = Database()
+
 # Returns response code from request
 response_code = requests.get(
     "https://www.rei.com/product/168251/rei-co-op-trailbreak-60-pack-mens")
@@ -13,22 +17,33 @@ soup = BeautifulSoup(response_html, "html.parser")
 # Product title
 # Hash syntax in select() when searching an id
 title = soup.select("#product-page-title")[0].get_text("|", True)
-Database.set_db(title)
 
-# Tech specs list of headers
+# Add product title as key in db
+db.set_db(title)
+
+# Lists of headers and their corresponding specs
 # Dot syntax in select() when searching a class
 headers_li = soup.select(".tech-specs__header")
+specs_li = soup.select(".tech-specs__value")
 
+# Properly formatted headers & specs without extra chars
 headers_li = [header.get_text("|", True) for header in headers_li]
+specs_li = [spec.get_text("|", True) for spec in specs_li]
+
+# Create product's spec dict to add to db
+product_specs = {}
+for i in range(len(headers_li)):
+    product_specs[headers_li[i]] = specs_li[i]
+
+# Complete product info in db
+db.set_db(title, product_specs)
+
+print(db.get_db())
+
 
 
 # TO DO:
-# - Figure out DB schema. Dict within Dict? List within List? List within Dict?
+# - Use OOP principles/encapsulation to segment code into different functions
+#   and files
 # - Eventually loop through all URL's on REI search page
 # - Use time module to slow things down?
-
-
-
-
-
-
