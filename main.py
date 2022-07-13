61e38399943bdb26d9532bdb4e8c0d797a4d7261
Sophia Lilienthal <sophialilienthal@icloud.com>
2022-07-13 15:36:57 -0600

    Implemented search page functionality to extract product links.

--- main.py
+++ main.py
@@ -8,33 +8,47 @@ from product import Product
 # Keys: Product titles Values: Product objects
 db = Database()
 
+# Create prefix to add to links
+prefix = "https://www.rei.com"
+
 # Returns response code from request
-response_code = requests.get(
-    "https://www.rei.com/product/168251/rei-co-op-trailbreak-60-pack-mens")
+response = requests.get("https://www.rei.com/c/backpacking-packs")
 
 # Represents html content from response
-response_html = response_code.content
-soup = BeautifulSoup(response_html, "html.parser")
+soup = BeautifulSoup(response.content, "html.parser")
 
-# Creates Product object and sets name of product
-product = Product()
-product.update_title(soup)
-title = product.get_title()
-product.update_specs(soup)
+# Gather all link elements from search page
+link_el = soup.find_all('a')
 
-# Add product to db
-db.update_db(title, product)
+# Create a list of links for products only
+links = []
+for el in link_el:
+    link = el["href"]
+    if link[0:9] == '/product/':
+        links.append(prefix + link)
 
-# Delay requests to 1 req/second
-sleep(1)
+# Extract product details from each link on search page and save them in db
+for link in links:
+    response = requests.get(link)
+    bs_obj = BeautifulSoup(response.content, "html.parser")
 
+    # Creates Product object and updates title/specs of product
+    product = Product()
+    product.update_title(bs_obj)
+    title = product.get_title()
+    product.update_specs(bs_obj)
 
-print(db.get_db())
+    # Add product to db as product title: product object key/val pairs
+    db.update_db(title, product)
 
+    # Delay requests to 1 req/second
+    sleep(1)
+
+print(db.get_db().keys())
+print(db.get_db()['Co-op Cycles DRT 1.1 Bike'].get_specs())
 
 
 # TO DO:
-# - Use OOP principles/encapsulation to segment code into different functions
-#   and files
-# - Eventually loop through all URL's on REI search page
-# - Use time module to slow things down?
+# - Set up actual DB for scraper
+# - Create Search(?) object to handle finding product links, adding products to
+# db, looping through links on search page
